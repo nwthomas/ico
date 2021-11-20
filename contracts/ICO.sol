19d0d6ae72b2fe7b7739c8fb563fd7e5c666ec89
Nathan Thomas <nwthomas@me.com>
2021-11-20 00:32:36 -0800

    Finish first draft of ICO and SpaceCoin contracts

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "./SpaceCoin.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/tokens/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title A contract for rolling out an ICO of SpaceCoin
@@ -10,64 +11,82 @@ import "@openzeppelin/contracts/tokens/SafeERC20.sol";
  * @notice This contract is not audited - Use at your own risk!
  */
 contract ICO is Ownable {
-  // Total supply is aiming for 150,000 tokens at 5/1 tokens/ether
-  uint256 private constant CONTRIBUTION_CAP = 30_000 ether;
+  // ICO is for 150,000 tokens at 5/1 tokens/ether
   uint256 private constant TOKEN_MULTIPLIER = 5;
-  uint256 private constant MAX_CONTRIBUTION_PER_ADDRESS = 1_000 ether;
 
-  bool private initialized = false;
-  bool public isPaused = false;
-  uint256 public raisedAmount = 0;
-  address public tokenAddress;
+  // Seed phase is 75,000 tokens with an individual cap
+  uint256 private constant SEED_CONTRIBUTIONS_PER_ADDRESS = 1_500 ether;
+  uint256 private constant SEED_CONTRIBUTIONS_CAP = 15_000 ether;
 
-  mapping(address => uint256) public addressToContributions;
+  // General phase is 75,000 tokens with an individual cap
+  uint256 private constant GENERAL_CONTRIBUTIONS_PER_ADDRESS = 1_000 ether;
+  uint256 private constant GENERAL_CONTRIBUTIONS_CAP = 30_000 ether;
 
   enum Phases {
     SEED,
     GENERAL,
     OPEN
   }
-  string public currentPhase = Phases.SEED;
 
-  event NewPhase(string indexed phase);
+  Phases public currentPhase = Phases.SEED;
+  bool private initialized = false;
+  bool public isPaused = false;
+  uint256 private raisedAmount = 0;
+  address public tokenAddress;
+
+  mapping(address => uint256) public addressToContributions;
+  mapping(address => bool) public approvedSeedInvestors;
+
+  event NewPhase(Phases indexed phase);
+
+  modifier hasContributions() {
+    require(
+      addressToContributions[msg.sender] > 0,
+      "ICO: address has no contributions"
+    );
+    _;
+  }
 
   modifier isNotPaused() {
     require(!isPaused, "ICO: the ICO is paused");
     _;
   }
 
+  modifier isOpenPhase() {
+    require(currentPhase == Phases.OPEN, "ICO: not open phase");
+    _;
+  }
+
   modifier isInitialized() {
-    require(isInitialized, "ICO: not initialized");
+    require(initialized, "ICO: not initialized");
     _;
   }
 
   modifier canContributeForCurrentPhase() {
     if (currentPhase == Phases.SEED) {
-      require(raisedAmount < 15_000 ether, "ICO: phase contributions reached");
       require(
-        addressToContributions[msg.sender] < 1_500 ether,
-        "ICO: address phase contributions reached"
+        raisedAmount < SEED_CONTRIBUTIONS_CAP,
+        "ICO: phase contributions reached"
+      );
+      require(
+        addressToContributions[msg.sender] < SEED_CONTRIBUTIONS_PER_ADDRESS,
+        "ICO: contribution maximum reached"
       );
     } else if (currentPhase == Phases.GENERAL) {
-      require(raisedAmount < 30_000 ether, "ICO: phase contributions reached");
       require(
-        addressToContributions[msg.sender] < 1_000 ether,
-        "ICO: address phase contributions reached"
-      );
-    } else {
-      require(
-        raisedAmount < CONTRIBUTION_CAP,
+        raisedAmount < GENERAL_CONTRIBUTIONS_CAP,
         "ICO: phase contributions reached"
+      );
+      require(
+        addressToContributions[msg.sender] < GENERAL_CONTRIBUTIONS_PER_ADDRESS,
+        "ICO: contribution maximum reached"
       );
     }
     _;
   }
 
-  constructor(
-    address memory _tokenAddress,
-    address[] memory _approvedSeedInvestors
-  ) {
-    require(_tokenAddress != 0, "ICO: address must be valid");
+  constructor(address _tokenAddress) {
+    require(_tokenAddress != address(0), "ICO: address must be valid");
     tokenAddress = _tokenAddress;
   }
 
@@ -78,17 +97,20 @@ contract ICO is Ownable {
     isNotPaused
     canContributeForCurrentPhase
   {
-    uint256 amountToReturn = _excessEtherToReturn(msg.value);
+    uint256 amountToReturn = _getExcessEtherToReturn(msg.value);
     uint256 validContributionAmount = msg.value - amountToReturn;
-    addressToContributions[msg.sender] += msg.value;
+    addressToContributions[msg.sender] += validContributionAmount;
     raisedAmount += validContributionAmount;
-
-    uint256 tokensBought = validContributionAmount * TOKEN_MULTIPLIER;
-    SafeERC20(tokenAddress).transfer(tokensBought);
 
     if (amountToReturn > 0) {
       (bool success, ) = msg.sender.call{ value: amountToReturn }("");
+      require(success, "ICO: excess funds transfer failed");
     }
+  }
+
+  function claimTokens() external isInitialized isOpenPhase hasContributions {
+    uint256 amountToTransfer = addressToContributions[msg.sender] * 5 * 10**18;
+    IERC20(tokenAddress).transfer(msg.sender, amountToTransfer);
   }
 
   /**
@@ -103,12 +125,9 @@ contract ICO is Ownable {
    * @dev The contract cannot move back in phases, only forward
    */
   function progressPhases() external onlyOwner isInitialized {
-    if (currentPhase == Phases.SEED) {
-      currentPhase = Phases.GENERAL;
-    } else {
-      currentPhase = Phases.OPEN;
-    }
+    require(currentPhase != Phases.OPEN, "ICO: phases complete");
 
+    currentPhase = Phases(uint256(currentPhase) + 1);
     emit NewPhase(currentPhase);
   }
 
@@ -120,14 +139,22 @@ contract ICO is Ownable {
   }
 
   /**
+   * @notice Toggles whether or not a given address is eligible for seed phase investment
+   */
+  function toggleSeedInvestor(address seedInvestor) external onlyOwner {
+    approvedSeedInvestors[seedInvestor] = !approvedSeedInvestors[seedInvestor];
+  }
+
+  /**
    * @notice Private method to calculate the exact amount of ether to return (if any)
    * @param _messageValue The ether derived from msg.value
    */
-  function _excessEtherToReturn(uint256 calldata _messageValue)
+  function _getExcessEtherToReturn(uint256 _messageValue)
     private
     view
     isInitialized
     isNotPaused
+    canContributeForCurrentPhase
     returns (uint256)
   {
     uint256 addressContributions = addressToContributions[msg.sender];
@@ -135,18 +162,25 @@ contract ICO is Ownable {
 
     if (currentPhase == Phases.SEED) {
       return
-        newContributionsTotal > 1_500 ether
-          ? currentContributions - 1_500 ether
+        newContributionsTotal > SEED_CONTRIBUTIONS_PER_ADDRESS
+          ? newContributionsTotal - SEED_CONTRIBUTIONS_PER_ADDRESS
           : 0;
     } else if (currentPhase == Phases.GENERAL) {
       return
-        newContributionsTotal > 1_000 ether
-          ? currentContributions - 1_000 ether
+        newContributionsTotal > GENERAL_CONTRIBUTIONS_PER_ADDRESS
+          ? newContributionsTotal - GENERAL_CONTRIBUTIONS_PER_ADDRESS
           : 0;
     }
 
-    // Covers general phase when the address caps no longer matter
+    // Covers general phase when the total ICO cap is all that matters
     uint256 newRaisedAmount = addressContributions + raisedAmount;
-    return newRaisedAmount > 30_000 ether ? newRaisedAmount - 30_000 : 0;
+    return
+      newRaisedAmount > GENERAL_CONTRIBUTIONS_CAP
+        ? newRaisedAmount - GENERAL_CONTRIBUTIONS_CAP
+        : 0;
+  }
+
+  receive() external payable {
+    revert("ICO: use buyTokens function");
   }
 }
--- contracts/SpaceCoin.sol
+++ contracts/SpaceCoin.sol
@@ -1,15 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract SpaceCoin {
-  bool public isTaxingTokens = true;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-  constructor(address[] memory _seedInvestors) {
-    // finish
+contract SpaceCoin is Ownable, ERC20 {
+  // While this is a constant, it matches the outcome of the decimals() function in ERC20
+  uint256 private constant DECIMAL_POINTS = 10**18;
+  uint256 public constant MAX_SUPPLY = 500_000 * DECIMAL_POINTS;
+  uint256 public constant TAX_PERCENTAGE = 2;
+  // ICO is aiming for 150,000 tokens at 5/1 tokens/ether
+  uint256 private constant TOKEN_MULTIPLIER = 5;
+
+  bool public isTaxingTokens = true;
+  address private treasuryAddress;
+
+  constructor(address _treasuryAddress, address _icoAddress)
+    ERC20("Space Coin", "SPC")
+  {
+    treasuryAddress = _treasuryAddress;
+    _mint(_icoAddress, MAX_SUPPLY);
   }
 
-  /// @notice Toggles the state varaibles that controls token tax on transactions
-  function toggleIsTaxingTokens() external {
-    isTaxingTokens = isTaxingTokens ? false : true;
+  /**
+   * @notice Toggles the state variables that controls token tax on transactions
+   */
+  function toggleIsTaxingTokens() external onlyOwner {
+    isTaxingTokens = !isTaxingTokens;
+  }
+
+  function _transfer(
+    address _to,
+    address _from,
+    uint256 _amount
+  ) internal virtual override {
+    require(_amount > 0, "SpaceCoin: must be valid amount");
+
+    if (isTaxingTokens) {
+      uint256 taxAmount = (_amount / 100) * TAX_PERCENTAGE;
+      _amount = _amount - taxAmount;
+      super._transfer(treasuryAddress, _from, taxAmount);
+    }
+
+    super._transfer(_to, _from, _amount);
+  }
+
+  function updateTreasuryAddress(address _newTreasuryAddress)
+    external
+    onlyOwner
+  {
+    require(
+      _newTreasuryAddress != address(0),
+      "SpaceCoin: address must not be 0"
+    );
+
+    treasuryAddress = _newTreasuryAddress;
   }
 }
