4787966b3fc461de7c1cc245ca68fccbcc120793
Nathan Thomas <nwthomas@me.com>
2021-11-22 20:00:00 -0600

    Initialize Rinkeby ICO contract

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,7 +8,7 @@ const TWITTER_HANDLE = "nwthomas_";
 const TWITTER_URL = `https://www.twitter.com/${TWITTER_HANDLE}`;
 
 function App() {
-  const { currentAccount } = useEthereum();
+  const { currentAccount, connectToWallet } = useEthereum();
   const buttonLabel = "Connect Wallet";
 
   return (
@@ -19,7 +19,7 @@ function App() {
           currentAccount={currentAccount ? currentAccount : undefined}
         />
         <h1>Space Coin ICO</h1>
-        <button>{buttonLabel}</button>
+        <button onClick={connectToWallet}>{buttonLabel}</button>
         <a href={TWITTER_URL}>{`Built by ${TWITTER_HANDLE}`}</a>
       </RootStyles>
     </>
